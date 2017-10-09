3fbd2c9a221d52386b96eb3c0d59ce6375101b39
dhirendra pratap <dhirendra@hometriangle.com>
2017-10-09 14:56:25 +0530

    php way of UI

---
+++ index.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Stack Multipurpose HTML Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Site Description Here">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/stack-interface.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/socicon.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/lightbox.min.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/flickity.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/iconsmind.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/jquery.steps.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/theme.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/custom.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    </head>
+    <body class=" ">
+        <a id="start"></a>
+        <?php include("./common/navbar.php"); ?>
+        <div class="main-container">
+            <section class="text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                            <h1>Build amazing websites fast.</h1>
+                            <p class="lead">
+                                A comprehensive guide to creating fast, responsive websites &mdash; including
+                                <br class="hidden-xs hidden-sm" /> input from 5 veteran designers and developers
+                            </p>
+                            <form action="//mrare.us8.list-manage.com/subscribe/post?u=77142ece814d3cff52058a51f&amp;id=f300c9cce8" data-success="Thanks for signing up.  Please check your inbox for a confirmation email." data-error="Please provide your name and email address and agree to the terms.">
+                                <div class="col-sm-4">
+                                    <input class="validate-required" type="text" name="NAME" placeholder="Your Name" />
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="validate-required validate-email" type="email" name="EMAIL" placeholder="Email Address" />
+                                </div>
+                                <div class="col-sm-4">
+                                    <button type="submit" class="btn btn--primary type--uppercase">Get The Ebook</button>
+                                </div>
+                                <div class="col-sm-12">
+                                    <input class="validate-required" type="checkbox" name="group[13737][1]" />
+                                    <span>I have read and agree to the
+                                        <a href="#">terms and conditions</a>
+                                    </span>
+                                </div>
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                    <input type="text" name="b_77142ece814d3cff52058a51f_f300c9cce8" tabindex="-1" value="">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="switchable">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-5 mt--3">
+                            <div class="testimonial testimonial-2">
+                                <div class="testimonial__body boxed boxed--border bg--secondary">
+                                    <p class="lead">
+                                        &ldquo;In all my years in design I haven't seen a more consistent and impressive e-book. We're in magnum opus territory here people. Make sure you grab a copy and stop wasting time doing things the old way &mdash; I guarantee, you won't regret it.&rdquo;
+                                    </p>
+                                </div>
+                                <div class="testimonial__image">
+                                    <img alt="Image" src="img/avatar-round-4.png" />
+                                    <h5>Robert S.</h5>
+                                    <span>Brisbane, AU</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 text-center">
+                            <img alt="Image" src="img/art-1.jpg" />
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="text-center bg--secondary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                            <h2>All the tools you'll need</h2>
+                            <p class="lead">
+                                Whether you’re building a welcome mat for your SaaS or a clean, corporate portfolio, Stack has your design needs covered.
+                            </p>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="bg--secondary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <div class="feature feature-2 boxed boxed--border">
+                                <i class="icon icon-Clock-Back color--primary"></i>
+                                <div class="feature__body">
+                                    <p>
+                                        Save time with a multitude of styled components designed to showcase your content
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature feature-2 boxed boxed--border">
+                                <i class="icon icon-Duplicate-Window color--primary"></i>
+                                <div class="feature__body">
+                                    <p>
+                                        Construct mockups or production-ready pages in-browser with Variant Page Builder
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature feature-2 boxed boxed--border">
+                                <i class="icon icon-Life-Jacket color--primary"></i>
+                                <div class="feature__body">
+                                    <p>
+                                        Take comfort in 6 months included support with a dedicated support forum
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature feature-2 boxed boxed--border">
+                                <i class="icon icon-Pen color--primary"></i>
+                                <div class="feature__body">
+                                    <p>
+                                        Save time with a multitude of styled components designed to showcase your content
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature feature-2 boxed boxed--border">
+                                <i class="icon icon-Pantone color--primary"></i>
+                                <div class="feature__body">
+                                    <p>
+                                        Construct mockups or production-ready pages in-browser with Variant Page Builder
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="feature feature-2 boxed boxed--border">
+                                <i class="icon icon-Fingerprint color--primary"></i>
+                                <div class="feature__body">
+                                    <p>
+                                        Take comfort in 6 months included support with a dedicated support forum
+                                    </p>
+                                </div>
+                            </div>
+                            <!--end feature-->
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="switchable feature-large bg--primary">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-xs-12">
+                            <div class="video-cover border--round box-shadow-wide">
+                                <div class="background-image-holder">
+                                    <img alt="image" src="img/landing-8.jpg" />
+                                </div>
+                                <div class="video-play-icon"></div>
+                                <iframe data-src="https://www.youtube.com/embed/6p45ooZOOPo?autoplay=1" allowfullscreen="allowfullscreen"></iframe>
+                            </div>
+                            <!--end video cover-->
+                        </div>
+                        <div class="col-sm-6 col-md-5">
+                            <div class="mt--2">
+                                <h2>The perfect resource for modern startups</h2>
+                                <p class="lead">
+                                    Launching an attractive and scalable website quickly and affordably is important for modern startups &mdash; Stack offers massive value without looking 'bargain-bin'.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                            <h2>Start building beautiful websites that convert
+                                <br class="hidden-xs hidden-sm" /> visitors into customers.</h2>
+                            <form action="//mrare.us8.list-manage.com/subscribe/post?u=77142ece814d3cff52058a51f&amp;id=f300c9cce8" data-success="Thanks for signing up.  Please check your inbox for a confirmation email." data-error="Please provide your name and email address and agree to the terms.">
+                                <div class="col-sm-4">
+                                    <input class="validate-required" type="text" name="NAME" placeholder="Your Name" />
+                                </div>
+                                <div class="col-sm-4">
+                                    <input class="validate-required validate-email" type="email" name="EMAIL" placeholder="Email Address" />
+                                </div>
+                                <div class="col-sm-4">
+                                    <button type="submit" class="btn btn--primary type--uppercase">Get The Ebook</button>
+                                </div>
+                                <div class="col-sm-12">
+                                    <input class="validate-required" type="checkbox" name="group[13737][1]" />
+                                    <span>I have read and agree to the
+                                        <a href="#">terms and conditions</a>
+                                    </span>
+                                </div>
+                                <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                                    <input type="text" name="b_77142ece814d3cff52058a51f_f300c9cce8" tabindex="-1" value="">
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+            <section class="text-center unpad--bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <img alt="Image" src="img/ebook-2.png" />
+                        </div>
+                    </div>
+                    <!--end of row-->
+                </div>
+                <!--end of container-->
+            </section>
+        <?php include("./common/footer.php"); ?>
+        </div>
+        <!--<div class="loader"></div>-->
+        <a class="back-to-top inner-link" href="#start" data-scroll-class="100vh:active">
+            <i class="stack-interface stack-up-open-big"></i>
+        </a>
+        <script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/flickity.min.js"></script>
+        <script src="js/easypiechart.min.js"></script>
+        <script src="js/parallax.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script src="js/datepicker.js"></script>
+        <script src="js/isotope.min.js"></script>
+        <script src="js/ytplayer.min.js"></script>
+        <script src="js/lightbox.min.js"></script>
+        <script src="js/granim.min.js"></script>
+        <script src="js/jquery.steps.min.js"></script>
+        <script src="js/countdown.min.js"></script>
+        <script src="js/twitterfetcher.min.js"></script>
+        <script src="js/spectragram.min.js"></script>
+        <script src="js/smooth-scroll.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ info.php
@@ -0,0 +1,10 @@
+
+<?php echo phpinfo(); ?> 
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
