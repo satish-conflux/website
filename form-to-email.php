84f0e616942ec479c4901189a6a5d7d8482e9ac0
Ubuntu <ubuntu@ip-172-31-29-143.ap-south-1.compute.internal>
2017-11-23 09:48:18 +0000

    email settings

--- form-to-email.php
+++ form-to-email.php
@@ -1,9 +1,9 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
-  $to = 'ashok@finflux.co,dhiru.iitm@gmail.com'; // Your e-mail address here.
-  $body = "\nName: {$_POST['name']}\nEmail: {$_POST['email']}\n\n\n{$_POST['message']}\n\n";
-  mail($to, "New contact request on finflux.co", $body); // E-Mail subject here.
-    }
-}
+  if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+$from='support @ Finflux';
+$headers .= 'From: ' . $from . "\r\n";
+    $to = 'binny@finflux.co,nayan@finflux.co'; // Your e-mail address here.
+    $body = "\nName: {$_POST['name']}\nEmail: {$_POST['email']}\n\n\n{$_POST['message']}\n\n";
+    mail($to, "New contact request on finflux.co", $body,$headers); // E-Mail subject here.
+  }
 ?>
